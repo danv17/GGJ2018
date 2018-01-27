a2d25abfd0a1906326d6a706db77474665d14e0e
Ian Wells <ianwellsh@hotmail.com>
2018-01-27 08:50:51 -0300

    .

--- GGJ2018/Assets/Scripts/rotacion_radio.cs
+++ GGJ2018/Assets/Scripts/rotacion_radio.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class rotacion_radio : MonoBehaviour {
-
+	
 	// Use this for initialization
 	void Start () {
 		
@@ -11,11 +11,14 @@ public class rotacion_radio : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKey(KeyCode.Z)){//presiona A y se mueve la izquierda
-			transform.Rotate(new Vector3(0,0,-1.0f));
-		}	
-		if(Input.GetKey(KeyCode.X)){//presiona A y se mueve la izquierda
-			transform.Rotate(new Vector3(0,0,1.0f));
-		}
+		//while(z!=180 | z!=-180 ){
+			if(Input.GetKey(KeyCode.Z)){//presiona A y se mueve la izquierda
+				//transform.Rotate(new Vector3(0,0,-1.0f));
+			transform.RotateAround(Vector3.zero,Vector3.forward,20 *Time.deltaTime);
+			}	
+			if(Input.GetKey(KeyCode.X)){//presiona A y se mueve la izquierda
+				transform.Rotate(new Vector3(0,0,1.0f));
+			}
+		//}
 	}	
 }
