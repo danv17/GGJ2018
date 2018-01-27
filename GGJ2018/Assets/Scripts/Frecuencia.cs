9aa8f02ebe55fbfa3ba0b8a83a8ee2bf914323fe
Ian Wells <ianwellsh@hotmail.com>
2018-01-27 10:59:37 -0300

    Movimiento perilla con barra roja[Falta terminar boxcollider2d component]

---
+++ GGJ2018/Assets/Scripts/Frecuencia.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Frecuencia : MonoBehaviour {
+
+	// Use this for initialization
+	public float speed = 10.0f;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		float translation = Input.GetAxis ("Horizontal") * speed;
+		if (Input.GetKey(KeyCode.Z))
+		{
+			transform.Translate (new Vector3(0.02f,0,0));
+		}
+		if (Input.GetKey(KeyCode.X))
+		{
+			transform.Translate (new Vector3(-0.02f,0,0));
+		}
+	}
+}
--- GGJ2018/Assets/Scripts/rotacion_radio.cs
+++ GGJ2018/Assets/Scripts/rotacion_radio.cs
@@ -5,20 +5,24 @@ using UnityEngine;
 public class rotacion_radio : MonoBehaviour {
 	
 	// Use this for initialization
+
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		
 		//while(z!=180 | z!=-180 ){
-			if(Input.GetKey(KeyCode.Z)){//presiona A y se mueve la izquierda
-				//transform.Rotate(new Vector3(0,0,-1.0f));
-			transform.RotateAround(Vector3.zero,Vector3.forward,20 *Time.deltaTime);
+			if(Input.GetKey(KeyCode.Z)){//presiona Z y rota a la izquierda
+				transform.Rotate(new Vector3(0,0,-1.0f));
+				
 			}	
-			if(Input.GetKey(KeyCode.X)){//presiona A y se mueve la izquierda
+			if(Input.GetKey(KeyCode.X)){//presiona X y rota a la derecha
 				transform.Rotate(new Vector3(0,0,1.0f));
 			}
 		//}
 	}	
+
 }
+
