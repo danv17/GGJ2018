973a801d44f0e88080d93eab29047cdcbbc35b2f
danv17 <daniel.ariz.s@gmail.com>
2018-01-26 22:46:25 -0300

    Player Wave Script

--- GGJ2018/Assets/Scripts/Player.cs
+++ GGJ2018/Assets/Scripts/Player.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+    public BoxCollider2D bc2d;
+    public Rigidbody2D rb2d;
+    public Animator anim;
+
 	// Use this for initialization
 	void Start () {
-		
+
+        bc2d = GetComponent<BoxCollider2D>();
+        rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (Input.GetKey(KeyCode.A))
+        {
+            anim.SetTrigger("playerChop");
+        }
 	}
 }
