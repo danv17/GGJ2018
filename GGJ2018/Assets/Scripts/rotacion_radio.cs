5d369fab530793d0e99798085d5a0ca7adace19d
Ian Wells <ianwellsh@hotmail.com>
2018-01-27 01:16:37 -0300

    .

---
+++ GGJ2018/Assets/Scripts/rotacion_radio.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotacion_radio : MonoBehaviour {
+
+	public float smooth = 2.0F;
+	public float tiltAngle = 30.0F;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.A)){//presiona A y se mueve la izquierda
+			float tiltAroundZ = Input.GetAxis("Horizontal") * tiltAngle;
+			Quaternion target = Quaternion.Euler(tiltAroundX, 0, tiltAroundZ);
+			transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
+		}	
+
+	}	
+}
